eb332b2bc5ba224f34c491c91709832bd85cc00a
Md. Shahriar Mahin <78980025+MahinShahriar@users.noreply.github.com>
2025-09-29 12:27:07 +0600

    Update settings.py

--- MahinVerse/settings.py
+++ MahinVerse/settings.py
@@ -92,7 +92,7 @@ if DB_URL:
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.environ.get("CACHE_LOCATION"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
